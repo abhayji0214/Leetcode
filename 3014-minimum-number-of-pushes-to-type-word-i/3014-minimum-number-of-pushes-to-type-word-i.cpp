d3721e24e0278e9ed637a677620d557a57c24c17
Abhay Singh <abhayji0214@gmail.com>
2026-07-30 21:56:11 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -5,7 +5,12 @@ public:
         int ans = 0;
         
         for (int i = 0; i < n; ++i) {
-            ans = ans + (i / 8) + 1;
+            if(i<8) ans+=1;
+          else if(i<16) ans+=2;
+          else if(i<24) ans+=3;
+          else ans+=4;
+
+
         }
         
         return ans;
